59ce8c238a92eca40b79a09c716f4699853246e5
LI Jinlin <18081569d@gmail.com>
2019-07-25 16:13:45 +0800

    add elpa

---
+++ elpa/web-mode-20190625.1951/web-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "web-mode" "20190625.1951" "major mode for editing web templates" '((emacs "23.1")) :commit "a723d3ecd3606d1c1948871f46fc8c8d7f879fe4" :keywords '("languages") :authors '(("Fran\303\247ois-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("Fran\303\247ois-Xavier Bois") :url "http://web-mode.org")
